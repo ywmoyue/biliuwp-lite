f108efeca88317b0def2063b9176694178b7a615
GD-Slime <82302542+GD-Slime@users.noreply.github.com>
2024-01-01 10:32:12 +0800

    修改了个人主页搜索视频为空时的报错反馈 (#457)
    修复个人主页搜索视频为空时的报错反馈

---
+++ src/BiliLite.UWP/Models/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BiliLite.Models.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string msg) : base(msg)
+        {
+        }
+    }
+}
