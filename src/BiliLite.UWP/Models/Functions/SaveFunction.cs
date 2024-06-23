9877038fc5303c82f9ac8c8a23aaa47ebd83eacf
ywmoyue <ywmoyue@163.com>
2024-06-23 10:09:38 +0800

    新增快捷键Ctrl+S保存收藏选择，保存视频收藏后自动隐藏收藏夹选择菜单

---
+++ src/BiliLite.UWP/Models/Functions/SaveFunction.cs
@@ -0,0 +1,16 @@
+﻿using BiliLite.Pages;
+using System.Threading.Tasks;
+
+namespace BiliLite.Models.Functions
+{
+    public class SaveFunction : IShortcutFunction
+    {
+        public string Name { get; } = "保存操作";
+
+        public async Task Action(object param)
+        {
+            if (!(param is ISavablePage page)) return;
+            await page.Save();
+        }
+    }
+}
