6d464d7ab86bd908e08bd934b8e59bb18ce338ed
ywmoyue <ywmoyue@163.com>
2024-09-19 19:56:05 +0800

    完善持续快进步进加速

--- src/BiliLite.UWP/Models/Functions/PositionForwardPressShortcutFunction.cs
+++ src/BiliLite.UWP/Models/Functions/PositionForwardPressShortcutFunction.cs
@@ -1,3 +1,4 @@
+using System;
 using BiliLite.Extensions;
 using BiliLite.Pages;
 using System.Threading.Tasks;
@@ -8,9 +9,12 @@ namespace BiliLite.Models.Functions
 {
     public class PositionForwardPressShortcutFunction : BaseShortcutFunction
     {
-        private const int DefaultStepTimeMs = 1000; // 默认步进时间为1秒
-        private int _currentStepTimeMs = DefaultStepTimeMs;
-        private int _stepPosition = 1; // 当前档位
+        private const int DEFAULT_STEP_TIME_SECOND = 3; // 默认步进时间为1秒
+        private int m_currentStepTimeSecond = DEFAULT_STEP_TIME_SECOND;
+        private const int INTERVAL_TIME = 500;
+        private int m_stepPosition = 1; // 当前档位
+        private const int STEP_POSITION_UP_TIMES = 5; // 每5次步进，挡位提升1档
+        private static readonly int[] _stepTimesForPositions = { 3, 5, 10, 15, 30, 60, 600, 1800, 3600 };
 
         public override string Name { get; } = "持续快进";
 
@@ -19,52 +23,50 @@ namespace BiliLite.Models.Functions
             if (!(param is PlayPage page)) return;
             if (ControlsExtensions.CheckFocusTextBoxNow()) return;
 
-            // 根据档位设置步进时间
-            SetStepTimeBasedOnPosition();
-
-            var timer = new Timer(_currentStepTimeMs);
+            var timer = new Timer(INTERVAL_TIME);
+            var elapsedTimes = 0;
             timer.Elapsed += (sender, e) =>
             {
                 page.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
                 {
-                    page.PositionForward();
+                    // 根据档位设置步进时间
+                    SetStepTimeBasedOnPosition();
+                    page.PositionForward(m_currentStepTimeSecond);
                 });
+                elapsedTimes++;
+                if ((1f * elapsedTimes) % STEP_POSITION_UP_TIMES == 0)
+                {
+                    m_stepPosition += 1;
+                }
             };
             timer.AutoReset = true;
-            (ReleaseFunction as PositionForwardReleaseShortcutFunction).Timer = timer;
+            (ReleaseFunction as PositionForwardReleaseShortcutFunction).Excute = ()=>
+            {
+                timer.Stop();
+                m_stepPosition = 1;
+            };
             timer.Start();
         }
 
-    private void SetStepTimeBasedOnPosition()
-    {
-        if (_stepPosition >= 1 && _stepPosition <= 4)
+        private void SetStepTimeBasedOnPosition()
         {
-            // 对于前四档，使用线性增长
-            _currentStepTimeMs = new[] { 1000, 3000, 5000, 10000 }[_stepPosition - 1];
+            m_currentStepTimeSecond = m_stepPosition > 0 && m_stepPosition <= _stepTimesForPositions.Length
+                ? _stepTimesForPositions[m_stepPosition - 1]
+                : _stepTimesForPositions[_stepTimesForPositions.Length - 1];
         }
-        else if (_stepPosition >= 5 && _stepPosition <= 9)
-        {
-            // 对于后五档，使用指数增长
-            _currentStepTimeMs = new[] { 30000, 60000, 600000, 1800000, 3600000 }[_stepPosition - 5];
-        }
-        else
-        {
-            _currentStepTimeMs = DefaultStepTimeMs;
-        }
-    }
 
         public override IShortcutFunction ReleaseFunction { get; } = new PositionForwardReleaseShortcutFunction();
     }
 
     public class PositionForwardReleaseShortcutFunction : BaseShortcutFunction
     {
-        public Timer Timer { get; set; }
+        public Action Excute { get; set; }
 
         public override string Name { get; } = "停止持续快进";
 
         public override async Task Action(object param)
         {
-            Timer?.Stop();
+            Excute?.Invoke();
         }
     }
 }
