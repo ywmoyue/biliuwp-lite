b934a73a30b42c39df30ccfe3adc53b51a62a4cf
ywmoyue <ywmoyue@163.com>
2024-11-17 11:01:54 +0800

    ColorSelectedConvert空值处理

--- src/BiliLite.UWP/Converters/ColorSelectedConvert.cs
+++ src/BiliLite.UWP/Converters/ColorSelectedConvert.cs
@@ -4,15 +4,14 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
 namespace BiliLite.Converters
 {
-    public class ColorSelecteConvert : IValueConverter
+    public class ColorSelectedConvert : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if (value.ToString() == parameter.ToString())
-            {
-                return new SolidColorBrush((Color)App.Current.Resources["SystemAccentColor"]);
-            }
-            return new SolidColorBrush((Color)App.Current.Resources["TextColor"]);
+            if (value == null) return new SolidColorBrush((Color)App.Current.Resources["TextColor"]);
+            return value.ToString() == parameter.ToString()
+                ? new SolidColorBrush((Color)App.Current.Resources["SystemAccentColor"])
+                : new SolidColorBrush((Color)App.Current.Resources["TextColor"]);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
