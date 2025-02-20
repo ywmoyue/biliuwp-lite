cfb12c992ab693f0a27426e914a7308aa719a76a
ywmoyue <ywmoyue@163.com>
2025-02-20 20:22:03 +0800

    #1060 本地关注直播间显示直播状态

---
+++ src/BiliLite.UWP/Converters/LiveStatusConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace BiliLite.Converters;
+
+public static class LiveStatusConverter
+{
+    public static string Convert(int status)
+    {
+        return status switch
+        {
+            0 => "未开播",
+            1 => "直播中",
+            2 => "轮播中",
+            _ => "未知"
+        };
+    }
+}
