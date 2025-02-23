c12e4892d702e5cc1463ed07285c7cd8ad58102d
ProJend <projend@outlook.com>
2025-02-23 21:31:15 +0800

    Add SingleCornerRadiusConverter

---
+++ src/BiliLite.UWP/Converters/SingleCornerRadiusConverter.cs
@@ -0,0 +1,36 @@
+﻿using JiebaNet.Segmenter.Common;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace BiliLite.Converters
+{
+    class SingleCornerRadiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var noCorner = new CornerRadius(0);
+            if (parameter.ToString().IsEmpty() || value == null || parameter == null)
+                return noCorner;
+
+            var roundCorner = value.ToString();
+            var radius = System.Convert.ToDouble(parameter);
+            double topLeft = 0, topRight = 0, bottomRight = 0, bottomLeft = 0;
+            if (roundCorner.Contains("1"))
+                topLeft = radius;
+            if (roundCorner.Contains("2"))
+                topRight = radius;
+            if (roundCorner.Contains("3"))
+                bottomRight = radius;
+            if (roundCorner.Contains("4"))
+                bottomLeft = radius;
+
+            return new CornerRadius(topLeft, topRight, bottomRight, bottomLeft);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
