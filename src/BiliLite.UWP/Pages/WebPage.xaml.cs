eb3d016ad20358e3b2ea60528084a76adc671783
ProJend <projend@outlook.com>
2025-03-13 12:23:39 +0800

    开发webpage

--- src/BiliLite.UWP/Pages/WebPage.xaml.cs
+++ src/BiliLite.UWP/Pages/WebPage.xaml.cs
@@ -2,19 +2,18 @@
 using BiliLite.Extensions.Notifications;
 using BiliLite.Models.Common;
 using BiliLite.Services;
-using BiliLite.ViewModels.Common;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.Web.WebView2.Core;
+using Newtonsoft.Json;
 using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
-using Newtonsoft.Json;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -27,32 +26,21 @@ namespace BiliLite.Pages
     {
         private readonly CookieService m_cookieService;
         private static readonly ILogger _logger = GlobalLogger.FromCurrentType();
-        private readonly WebPageViewModel m_viewModel;
         private bool m_needCookie = false;
 
         public WebPage()
         {
-            m_viewModel = App.ServiceProvider.GetRequiredService<WebPageViewModel>();
             m_cookieService = App.ServiceProvider.GetRequiredService<CookieService>();
             this.InitializeComponent();
             Title = "网页浏览";
             this.Loaded += WebPage_Loaded;
-        }
-
-        private async void CoreWebView2_NewWindowRequested(CoreWebView2 sender, CoreWebView2NewWindowRequestedEventArgs args)
-        {
-            args.Handled = true;
-            var handelUrlResult = await MessageCenter.HandelUrl(args.Uri);
-            if (handelUrlResult) return;
-            var md = new MessageDialog("是否使用外部浏览器打开此链接？");
-            md.Commands.Add(new UICommand("确定", new UICommandInvokedHandler(async (e) => { await Windows.System.Launcher.LaunchUriAsync(new Uri(args.Uri)); })));
-            md.Commands.Add(new UICommand("取消", new UICommandInvokedHandler((e) => { })));
-            await md.ShowAsync();
+            DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
+            dataTransferManager.DataRequested += DataTransferManager_DataRequested;
         }
 
         private void WebPage_Loaded(object sender, RoutedEventArgs e)
         {
-            if (!(this.Parent is MyFrame frame)) return;
+            if (this.Parent is not MyFrame frame) return;
             frame.ClosedPage -= WebPage_ClosedPage;
             frame.ClosedPage += WebPage_ClosedPage;
         }
@@ -76,14 +64,14 @@ namespace BiliLite.Pages
             await InitWebView2();
             if (e.NavigationMode != NavigationMode.New) return;
 
-            var uri = "";
+            string uri;
             try
             {
                 var webPageNavigationInfo = JsonConvert.DeserializeObject<WebPageNavigationInfo>(JsonConvert.SerializeObject(e.Parameter));
                 uri = webPageNavigationInfo.Url;
                 m_needCookie = webPageNavigationInfo.NeedCookie;
             }
-            catch 
+            catch
             {
                 uri = e.Parameter.ToString();
             }
@@ -127,6 +115,28 @@ namespace BiliLite.Pages
         {
             await webView.EnsureCoreWebView2Async();
             webView.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+            webView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+        }
+
+        private async void CoreWebView2_NewWindowRequested(CoreWebView2 sender, CoreWebView2NewWindowRequestedEventArgs args)
+        {
+            args.Handled = true;
+            var handelUrlResult = await MessageCenter.HandelUrl(args.Uri);
+            if (handelUrlResult) return;
+            await NotificationShowExtensions.ShowMessageDialog("是否使用外部浏览器打开此链接？", "", new Uri(args.Uri));
+        }
+
+        private void CoreWebView2_NavigationStarting(CoreWebView2 sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            string targetUrl = args.Uri;
+            string pattern = @"^https://([a-zA-Z0-9]{2,})\.bilibili\.com";
+            if (!Regex.IsMatch(targetUrl, pattern))
+            {
+                NotificationShowExtensions.ShowMessageToast("检测到未知的重定向，已自动切换到移动版网站");
+
+                args.Cancel = true;
+                UseMoblieUserAgent();
+            }
         }
 
         protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
@@ -139,46 +149,11 @@ namespace BiliLite.Pages
             base.OnNavigatingFrom(e);
         }
 
-
-        private void btnForword_Click(object sender, RoutedEventArgs e)
-        {
-            if (webView.CanGoForward)
-            {
-                webView.GoForward();
-            }
-        }
-
-        private void btnRefresh_Click(object sender, RoutedEventArgs e)
-        {
-            webView.Reload();
-        }
-
-        private void btnBack_Click(object sender, RoutedEventArgs e)
-        {
-            if (webView.CanGoBack)
-            {
-                webView.GoBack();
-            }
-        }
-
-        private void btnShare_Click(object sender, RoutedEventArgs e)
-        {
-            DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
-            dataTransferManager.DataRequested -= DataTransferManager_DataRequested;
-            dataTransferManager.DataRequested += DataTransferManager_DataRequested;
-            DataTransferManager.ShowShareUI();
-        }
-
         private void DataTransferManager_DataRequested(DataTransferManager sender, DataRequestedEventArgs e)
         {
             DataPackage dataPackage = e.Request.Data;
             dataPackage.Properties.Title = "共享链接";
             dataPackage.SetWebLink(webView.Source);
-        }
-
-        private async void btnOpenBrowser_Click(object sender, RoutedEventArgs e)
-        {
-            await Windows.System.Launcher.LaunchUriAsync(webView.Source);
         }
 
         // webview2中没找到代替的api，暂时不用
@@ -200,9 +175,56 @@ namespace BiliLite.Pages
             }
         }
 
-        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
         {
-            NotificationShowExtensions.ShowMessageToast("虽然看起来像个浏览器，但这完全这不是个浏览器啊！ ╰（‵□′）╯");
+            FrameworkElement button = sender as FrameworkElement;
+            switch (button.Tag as string)
+            {
+                case "Back":
+                    if (webView.CanGoBack)
+                        webView.GoBack();
+                    break;
+                case "Forword":
+                    if (webView.CanGoForward)
+                        webView.GoForward();
+                    break;
+                case "Refresh":
+                    webView.Reload();
+                    break;
+                case "Info":
+                    NotificationShowExtensions.ShowMessageToast("虽然看起来像个浏览器，但这完全这不是个浏览器啊！ ╰（‵□′）╯");
+                    break;
+                case "Desktop":
+                    UseMoblieUserAgent(false);
+                    break;
+                case "Mobile":
+                    UseMoblieUserAgent();
+                    break;
+                case "Share":
+                    DataTransferManager.ShowShareUI();
+                    break;
+                case "OpenBrowser":
+                    await Windows.System.Launcher.LaunchUriAsync(webView.Source);
+                    break;
+            }
+        }
+
+        private readonly string desktopUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36";
+        private readonly string mobileUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/537.36";
+
+        private void UseMoblieUserAgent(bool isMobile = true)
+        {
+            if (isMobile)
+            {
+                webView.CoreWebView2.Settings.UserAgent = mobileUserAgent;
+                Moblie.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                webView.CoreWebView2.Settings.UserAgent = desktopUserAgent;
+                Moblie.Visibility = Visibility.Visible;
+            }
+            webView.Reload();
         }
 
         private void WebView_OnNavigationStarting(WebView2 sender, CoreWebView2NavigationStartingEventArgs args)
@@ -218,7 +240,7 @@ namespace BiliLite.Pages
                 {
                     tabViewItem.Header = webView.CoreWebView2.DocumentTitle;
                 }
-                else if (!(frame.Parent is TabViewItem))
+                else if (frame.Parent is not TabViewItem)
                 {
                     MessageCenter.ChangeTitle(this, webView.CoreWebView2.DocumentTitle);
                 }
@@ -262,9 +284,44 @@ $('.author-container').css('margin','12px 0px -12px 0px');"
             {
                 if (webView != null) webView.Visibility = Visibility.Visible;
             }
+        }
 
-            m_viewModel.IsEnableGoBack = webView.CanGoBack;
-            m_viewModel.IsEnableGoForward = webView.CanGoForward;
+        private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            string pattern = @"^(https?:\/\/)?([a-zA-Z0-9-]+\.)?([a-zA-Z0-9-]+\.)[a-zA-Z]{2,3}$";
+            if (Regex.IsMatch(AutoSuggestBox.Text, pattern))
+            {
+                if (AutoSuggestBox.Text.Contains("bilibili.com"))
+                {
+                    webView.Source = new Uri(AutoSuggestBox.Text);
+                }
+                else
+                {
+                    NotificationShowExtensions.ShowMessageToast("仅支持浏览B站网页");
+                }
+            }
+            else if (!string.IsNullOrEmpty(AutoSuggestBox.Text))
+            {
+                var searchUri = $"https://search.bilibili.com/all?keyword={AutoSuggestBox.Text}";
+                webView.Source = new Uri(searchUri);
+            }
+            else
+            {
+                webView.Source = new Uri(AutoSuggestBox.PlaceholderText);
+            }
+        }
+
+        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            string pattern = @"^(https?:\/\/)?([a-zA-Z0-9-]+\.)?([a-zA-Z0-9-]+\.)[a-zA-Z]{2,3}$";
+            if (Regex.IsMatch(sender.Text, pattern))
+            {
+                sender.QueryIcon = new SymbolIcon(Symbol.Go);
+            }
+            else
+            {
+                sender.QueryIcon = new SymbolIcon(Symbol.Find);
+            }
         }
     }
 }
