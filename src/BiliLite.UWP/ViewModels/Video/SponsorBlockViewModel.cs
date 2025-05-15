65d545eadb51d6db53623642529684cf8403333a
GD_ Slime <1289744583@qq.com>
2025-05-16 00:13:19 +0800

    加入SponsorBlock的API与VM

---
+++ src/BiliLite.UWP/ViewModels/Video/SponsorBlockViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BiliLite.ViewModels.Video
+{
+    public class SponsorBlockSegment
+    {
+        public float[] Segment { get; set; }
+        public string Cid { get; set; }
+        public string Uuid { get; set; }
+        public string Category { get; set; }
+        public string ActionType { get; set; }
+        public int Locked { get; set; }
+        public int Votes { get; set; }
+        public float VideoDuration { get; set; }
+    }
+
+    public class SponsorBlockVideo
+    {
+        public string VideoId { get; set; }
+        public List<SponsorBlockSegment> Segments { get; set; }
+    }
+
+    public class SponsorBlockViewModel
+    {
+        public List<SponsorBlockVideo> Videos { get; set; }
+    }
+}
