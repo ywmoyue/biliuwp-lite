6c884e13faaf19d7ca2a9b052dc47744a9371864
ywmoyue <28388300+ywmoyue@users.noreply.github.com>
2025-08-27 23:45:33 +0800

    #1297 禁用转发动态中的ListView滚动行为

---
+++ src/BiliLite.UWP/Extensions/DisableScrollExtension.cs
@@ -0,0 +1,50 @@
+﻿using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+
+namespace BiliLite.Extensions
+{
+    public static class DisableScrollExtension
+    {
+        public static readonly DependencyProperty DisableScrollProperty =
+        DependencyProperty.RegisterAttached(
+            "DisableScroll",
+            typeof(bool),
+            typeof(DisableScrollExtension),
+            new PropertyMetadata(false, OnDisableScrollChanged));
+
+        public static bool GetDisableScroll(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(DisableScrollProperty);
+        }
+
+        public static void SetDisableScroll(DependencyObject obj, bool value)
+        {
+            obj.SetValue(DisableScrollProperty, value);
+        }
+
+        private static void OnDisableScrollChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not FrameworkElement element)
+                return;
+
+            if ((bool)e.NewValue)
+            {
+                element.Dispatcher.RunAsync(CoreDispatcherPriority.Low,() =>
+                {
+                    var scrollViewer = FindScrollViewer(element);
+                    if (scrollViewer != null)
+                    {
+                        scrollViewer.VerticalScrollMode = ScrollMode.Disabled;
+                        scrollViewer.HorizontalScrollMode = ScrollMode.Disabled;
+                    }
+                });
+            }
+        }
+
+        private static ScrollViewer FindScrollViewer(DependencyObject parent)
+        {
+            return parent?.FindFirstChildByType<ScrollViewer>();
+        }
+    }
+}
