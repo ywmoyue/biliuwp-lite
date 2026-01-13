3cca343fc9d89f1e63de1c2a5f6a7f04a31d246d
ywmoyue <28388300+ywmoyue@users.noreply.github.com>
2026-01-13 22:30:39 +0800

    #1376 抽象下载器代码

--- src/BiliLite.UWP/Startup.cs
+++ src/BiliLite.UWP/Startup.cs
@@ -33,6 +33,7 @@ namespace BiliLite
             services.AddControls();
             services.AddDanmakuController();
 
+            services.AddSingleton<IDownloadService, BackgroundDownloadService>();
             services.AddSingleton<DownloadService>();
             services.AddSingleton<CookieService>();
             services.AddSingleton<ShortcutKeyService>();
